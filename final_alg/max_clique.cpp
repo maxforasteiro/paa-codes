2a5650636ab2f70996c2fe4d0048045539199c0e
Max Forasteiro <max.forasteiro@gmail.com>
2017-06-25 19:04:12 -0300

    Adding program with polynomial solution

--- final_alg/max_clique.cpp
+++ final_alg/max_clique.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
 using namespace std;
 const int MAX = 1000;
-int a[MAX+1][MAX+1]; //Matriz de adjacência L
-int x[MAX + 1]; //Solução atual
-int bestx[MAX + 1]; //Solução ótima
-int cn; //Número atual de vértices
-int bestn; //Ńúmero máximo atual de vértices
+int graph[MAX+1][MAX+1]; //Matriz de adjacência L
+int current_clique[MAX + 1]; //Solução atual
+int clique[MAX + 1]; //Solução ótima
+int current_size; //Número atual de vértices
+int clique_size; //Ńúmero máximo atual de vértices
 
-void backtrack(int i, int n){
+void backtrack(int level, int vertices){
   //Calcular grupo máximo
-  if(i > n){
-    for(int j=0;j<n;j++){
-      bestx[j] = x[j];
+  if(level > vertices){
+    for(int j=0;j<vertices;j++){
+      clique[j] = current_clique[j];
     }
-    bestn = cn;
+    clique_size = current_size;
     return;
   }
   //Verifica a conexão do vértice atual com os demais vértices
-  int OK = 1;
-  for(int j=0;j<i;j++){
-    if(x[j] && a[i][j] == 0){
+  int connected = 1;
+  for(int j=0;j<level;j++){
+    if(current_clique[j] && graph[level][j] == 0){
       //i e j não estão ligados
-      OK = 0;
+      connected = 0;
       break;
     }
   }
-  if(OK) { //sub-árvore esquerda
-    x[i] = 1;
-    cn++;
-    backtrack(i + 1, n);
-    x[i] = 0;
-    cn --;
+  if(connected){
+    current_clique[level] = 1;
+    current_size++;
+    backtrack(level + 1, vertices);
+    current_clique[level] = 0;
+    current_size--;
   }
-  if(cn + n - i > bestn){
-    x[i] = 0;
-    backtrack(i + 1, n);
+  if(current_size + vertices - level > clique_size){
+    current_clique[level] = 0;
+    backtrack(level + 1, vertices);
   }
 }
 
@@ -46,20 +46,20 @@ int main(){
 
   for(int i=0;i<e;i++){
     cin >> coord_x >> coord_y;
-    a[coord_x][coord_y] = a[coord_y][coord_x] = 1; //Matriz de adjacência
+    graph[coord_x][coord_y] = graph[coord_y][coord_x] = 1; //Matriz de adjacência
   }
 
   backtrack(0, v);
 
   for(int i=0;i<v;i++){
-    if(bestx[i]){
+    if(clique[i]){
       cout<<i<<" "; //Grupo de saída máxima
     }
   }
 
   cout<<endl;
 
-  cout<<"Maior grupo de vértices:"<<bestn<<endl;
+  cout<<"Maior grupo de vértices:"<<clique_size<<endl;
 
   return 0;
 }
--- final_alg/maximal_clique.cpp
+++ final_alg/maximal_clique.cpp
@@ -1,62 +1,112 @@
+//
+//  max_clique_p.cpp
+//  WS_C
+//
+//  Created by Lucas Pellizon on 6/24/17.
+//  Copyright © 2017 Lucas Pellizon. All rights reserved.
+//
+
 #include <iostream>
-#include <vector>
-#define MAX 1000
+#include <stdio.h>
+#include <cmath>
+#include <cstring>
+#include <stdlib.h>
+#include <algorithm>
+#include <limits.h>
+#include <list>
+
 using namespace std;
-
-long long count, N;
-long long graph[MAX][MAX], ne[MAX];
-vector<long long> clique;
-
-void BronKerbosch(long long r, long long p, long long x){
-  if((p == 0LL) && (x == 0LL)){
-    clique.push_back(r);
-  }
-  else{
-    int u=0;
-    for(; u<N; u++){
-      if((p | x) & (1LL << u)){
-        break;
-      }
+int **adj,m,*s;
+int clique(int s[],int l)
+{
+    int i,j,deg[l],mini = 0;
+    while(1)
+    {
+        int minn=l+5,maxx=0;
+        memset(deg,0,sizeof(deg));
+        for(i=0;i<l;i++)
+        {
+            for(j=0;j<l;j++)
+            {
+                if(adj[s[i]][s[j]] && i!=j)
+                {
+                    deg[i]++;
+                    //             cout<<"i="<<s[i]<<" j="<<s[j]<<endl;
+                }
+            }
+            
+        }
+        /*    cout<<"deg==>"<<endl;
+         for(i=0;i<l;i++)
+         cout<<deg[i]<<" ";
+         cout<<endl;
+         */    for(i=0;i<l;i++)
+         {
+             if(deg[i]<minn)
+                 minn=deg[i],mini=i;
+             if(deg[i]>maxx)
+                 maxx=deg[i];
+         }
+        //    cout<<"min="<<minn<<" max="<<maxx<<endl;
+        //   system("pause");
+        if(minn==maxx)
+            return l;
+        else
+        {
+            l--;
+            for(i=1;i<mini;i++)
+                s[i]=s[i];
+            for(;i<=l;i++)
+                s[i]=s[i+1];
+        }
     }
-    for(int i=0; i<N; i++){
-      if((p & ~ne[u]) & (1LL << i)){
-        BronKerbosch(r | (1LL << i), p & ne[i], x & ne[i]);
-        p -= (1LL << i);
-        x |= (1LL << i);
-      }
-    }
-  }
 }
+int main()
+{
+    int m,i,j,maxx=0;
+    int v, e;
+    int x, y;
+    scanf("%d", &v);//Número de vértices
+    scanf("%d", &m); //Número de arestas
+    
+    s = (int*) malloc(m*sizeof(int));
+    adj = (int**) malloc(m*sizeof(int*));
+    for (i=0;i<m;i++){
+        adj[i] = (int*) malloc(m*sizeof(int));
 
-int normalize_answer(int p){
-  int answer = 0, i = 0;
-  while(p){
-    if(p & 1LL){
-      answer += graph[i / N][i % N];
     }
-    p >>= 1;
-    i++;
-  }
-}
 
-int main(){
-  int v, x, y;
-  cin >> N >> v;
-  for(int i=0;i<v;i++){
-    cin >> x >> y;
-    graph[x][y] = graph[y][x] = 1;
-    ne[x] |= (1LL << y);
-  }
-
-  BronKerbosch(0, ~0LL, 0);
-
-  int answer = -1;
-  for(int i=0; i<clique.size(); i++){
-    if(3*clique[i] >= 2*N){
-      answer = max(answer, normalize_answer(clique[i]));
-      // cout << clique[i] << ' ';
+    
+    for(int i=0;i<m;i++){
+        cin>>x;
+        cin>>y;
+//        grauvertice[x]++;
+//        grau_vertice[y]++;
+        adj[x][y] = adj[y][x] = 1; //Matriz de adjacência
     }
-  }
-
-  return 0;
+//    cin>>m;
+//    for(i=0;i<m;i++)
+//    {
+//        for(j=0;j<m;j++)
+//            cin>>adj[i][j];
+//    }
+    /*  for(i=0;i<m;i++)
+     {
+     for(j=0;j<m;j++)
+     cout<<adj[i][j]<<" ";
+     cout<<endl;
+     }
+     */
+    for(i=0;i<m;i++)
+     {
+         int s[867]={i},len=1;
+         //    cout<<"i===>"<<i+1<<endl;
+         for(j=0;j<m;j++)
+             if(adj[i][j])
+                 s[len++]=j;//,cout<<j+1<<" ";
+         //  cout<<"len="<<len<<endl;
+         maxx=max(maxx,clique(s,len));
+     }
+    cout<<"clique maximo: " << maxx << "\n";
+    return 0;
 }
